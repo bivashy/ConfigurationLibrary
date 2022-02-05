847ff91ef63d8075a28f695b6b3c95c936548fde
Ubivashka <Ubivashka@gmail.com>
2022-02-05 22:21:50 +0600

    Fixed configuration context compability with dealership

--- spigot/src/main/java/com/ubivashka/config/converters/SpigotConfigurationListConverter.java
+++ spigot/src/main/java/com/ubivashka/config/converters/SpigotConfigurationListConverter.java
@@ -2,10 +2,10 @@ package com.ubivashka.config.converters;
 
 import org.bukkit.configuration.ConfigurationSection;
 
-import com.ubivashka.config.processors.context.IConfigurationContext;
+import com.ubivashka.config.contexts.SpigotConfigurationContext;
 
 public abstract class SpigotConfigurationListConverter<V, E>
-		extends ConfigurationListConverter<ConfigurationSection, IConfigurationContext<ConfigurationSection>, V, E> {
+		extends ConfigurationListConverter<ConfigurationSection, SpigotConfigurationContext, V, E> {
 
 	public SpigotConfigurationListConverter(Class<E> valueClass) {
 		super(valueClass);
