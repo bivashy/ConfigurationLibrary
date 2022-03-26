b37a344f84628b9c4503887300b6e4d3ccf50372
Ubivashka <Ubivashka@gmail.com>
2022-03-26 20:21:18 +0600

    Initialize another configuration classes

--- core/src/main/java/com/ubivashka/config/processors/ConfigurationHolder.java
+++ core/src/main/java/com/ubivashka/config/processors/ConfigurationHolder.java
@@ -1,6 +1,10 @@
 package com.ubivashka.config.processors;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.ubivashka.config.annotations.ConfigField;
 import com.ubivashka.config.annotations.ConverterType;
@@ -11,8 +15,12 @@ public abstract class ConfigurationHolder<T, S extends IConfigurationSectionHold
 	private S configurationSectionHolder;
 
 	public void init(S configurationSectionHolder) {
+		init(configurationSectionHolder, this);
+	}
+
+	public void init(S configurationSectionHolder, Object... objects) {
 		this.configurationSectionHolder = configurationSectionHolder;
-		setupFields();
+		initFields(Arrays.stream(objects).collect(Collectors.toMap(object -> object.getClass(), Function.identity())));
 	}
 
 	protected abstract ConfigurationContextProcessorsDealership<T, C, ? extends IConfigurationContextProcessor<T, C>> getConfigurationFieldProcessorsDealership();
@@ -23,31 +31,25 @@ public abstract class ConfigurationHolder<T, S extends IConfigurationSectionHold
 	protected void handleError(Field field, C context, Exception e) {
 	}
 
-	@SuppressWarnings("unchecked")
-	private void setupFields() {
-		Class<?> clazz = getClass();
-		while (clazz != null && ConfigurationHolder.class.isAssignableFrom(clazz)) {
+	private void initFields(Map<Class<?>, Object> classMap) {
+		classMap.keySet().forEach(clazz -> {
 
-			setupFields((Class<? extends ConfigurationHolder<T, S, C>>) getClass());
+			while (clazz != null && clazz != Object.class) {
 
-			Class<?> superClazz = clazz.getSuperclass();
-			while (superClazz != null && ConfigurationHolder.class.isAssignableFrom(superClazz)) {
+				setupFields(clazz, classMap.get(clazz));
 
-				setupFields((Class<? extends ConfigurationHolder<T, S, C>>) superClazz);
-
-				superClazz = superClazz.getSuperclass();
+				clazz = clazz.getSuperclass();
 			}
 
-			clazz = clazz.getEnclosingClass();
-		}
+		});
 	}
 
-	private void setupFields(Class<? extends ConfigurationHolder<T, S, C>> clazz) {
-		for (Field f : clazz.getDeclaredFields())
-			setupField(f, clazz);
+	private void setupFields(Class<?> clazz, Object fieldHolder) {
+		for (Field field : clazz.getDeclaredFields())
+			setupField(field, clazz, fieldHolder);
 	}
 
-	private void setupField(Field field, Class<? extends ConfigurationHolder<T, S, C>> clazz) {
+	private void setupField(Field field, Class<?> clazz, Object fieldHolder) {
 		if (!field.isAnnotationPresent(ConfigField.class))
 			return;
 
@@ -76,7 +78,7 @@ public abstract class ConfigurationHolder<T, S extends IConfigurationSectionHold
 			return;
 
 		try {
-			field.set(this, configurationFieldContext.getCurrentObject());
+			field.set(fieldHolder, configurationFieldContext.getCurrentObject());
 		} catch (IllegalArgumentException | IllegalAccessException e) {
 			handleError(field, configurationFieldContext, e);
 		}
