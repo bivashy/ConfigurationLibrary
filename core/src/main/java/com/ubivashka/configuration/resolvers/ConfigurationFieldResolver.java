86a0649f88452a04266c9794241aa65cf405310c
Ubivashka <Ubivashka@gmail.com>
2022-04-10 21:14:20 +0600

    Documentation of shouldResolveCollection method

--- core/src/main/java/com/ubivashka/configuration/resolvers/ConfigurationFieldResolver.java
+++ core/src/main/java/com/ubivashka/configuration/resolvers/ConfigurationFieldResolver.java
@@ -5,6 +5,12 @@ import com.ubivashka.configuration.contexts.ConfigurationFieldResolverContext;
 public interface ConfigurationFieldResolver<T> {
 	T resolveField(ConfigurationFieldResolverContext resolverContext);
 
+	/**
+	 * Defines should resolver process collection in configuration.
+	 * Will not work if the <b>custom</b> factory itself does not handle it.
+	 * 
+	 * @return Should process configuration list.
+	 */
 	default boolean shouldResolveCollection() {
 		return true;
 	}
