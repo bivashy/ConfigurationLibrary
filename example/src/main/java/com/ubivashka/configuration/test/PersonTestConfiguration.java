2b37a066b6b191be0e6131e5d0f157dad6d4f09b
U61vashka <botyrbojey@gmail.com>
2022-08-01 18:14:19 +0600

    Additional empty,default list example

--- example/src/main/java/com/ubivashka/configuration/test/PersonTestConfiguration.java
+++ example/src/main/java/com/ubivashka/configuration/test/PersonTestConfiguration.java
@@ -1,5 +1,7 @@
 package com.ubivashka.configuration.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.configuration.ConfigurationSection;
@@ -9,59 +11,69 @@ import com.ubivashka.configuration.annotation.ConfigField;
 import com.ubivashka.configuration.holder.ConfigurationSectionHolder;
 
 public class PersonTestConfiguration {
-	@ConfigField("section-test")
-	private List<Person> personsList;
-	@ConfigField("section-test.hi")
-	private Person artem;
+    @ConfigField("section-test")
+    private List<Person> personsList;
+    @ConfigField("section-test.hi")
+    private Person artem;
+    @ConfigField("null-section")
+    private List<Person> emptyPersonsList = new ArrayList<>();
+    @ConfigField("null-section")
+    private List<Person> defaultPersonsList = new ArrayList<>(Arrays.asList(new Person("null", "null", -1)));
+    @ConfigField("section-test.many")
+    private List<PersonId> idObjects;
+    @ConfigField("section-test.single")
+    private PersonId idObject;
 
-	@ConfigField("section-test.many")
-	private List<PersonId> idObjects;
-	@ConfigField("section-test.single")
-	private PersonId idObject;
-
-	public PersonTestConfiguration(ConfigurationSection section) {
-		TestMain.getConfigurationProcessor().resolve(section, this);
-		System.out.println(this);
-	}
+    public PersonTestConfiguration(ConfigurationSection section) {
+        TestMain.getConfigurationProcessor().resolve(section, this);
+        System.out.println(this);
+    }
 
 	@Override
 	public String toString() {
-		return "PersonTestConfiguration [personsList=" + personsList + ", artem=" + artem + ", idObjects=" + idObjects + ", idObject=" + idObject + "]";
+		return "PersonTestConfiguration{" +
+				"personsList=" + personsList +
+				", artem=" + artem +
+				", emptyPersonsList=" + emptyPersonsList +
+				", defaultPersonsList=" + defaultPersonsList +
+				", idObjects=" + idObjects +
+				", idObject=" + idObject +
+				'}';
 	}
 
 	public static class PersonId {
-		private final String id;
+        private final String id;
 
-		public PersonId(String id) {
-			this.id = id;
-		}
+        public PersonId(String id) {
+            this.id = id;
+        }
 
-		@Override
-		public String toString() {
-			return "PersonId [id=" + id + "]";
-		}
+        @Override
+        public String toString() {
+            return "PersonId [id=" + id + "]";
+        }
 
-	}
+    }
 
-	public static class Person implements ConfigurationHolder {
-		private final String name;
-		private final String subName;
-		private int age;
+    public static class Person implements ConfigurationHolder {
+        private final String name;
+        private final String subName;
+        private int age;
 
-		private Person(String name, String subName, int age) {
-			this.name = name;
-			this.subName = subName;
-			this.age = age;	
-		}
+        private Person(String name, String subName, int age) {
+            this.name = name;
+            this.subName = subName;
+            this.age = age;
+        }
 
-		public Person(ConfigurationSectionHolder personSection) {
-			this(personSection.getString("name"), personSection.getString("subname"), personSection.getInt("age"));
-		}
+        public Person(ConfigurationSectionHolder personSection) {
+            this(personSection.getString("name"), personSection.getString("subname"), personSection.getInt("age"));
+        }
 
-		@Override
-		public String toString() {
-			return "Person [name=" + name + ", subName=" + subName + ", age=" + age + "]";
-		}
+        @Override
+        public String toString() {
+            return "Person [name=" + name + ", subName=" + subName + ", age=" + age + "]";
+        }
 
-	}
+    }
 }
